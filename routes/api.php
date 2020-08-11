93cd054f0b7b97571c536dbf546f5a04aae8963a
indrapwa_laptop <gensbookcities@gmail.com>
2020-08-11 22:55:47 +0700

    part5 laravel api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/todos', 'TodoController@index');
+Route::post('/todos', 'TodoController@store');
+Route::patch('/todos/{todo}', 'TodoController@update');
+Route::patch('/todosCheckAll', 'TodoController@updateAll');
+Route::delete('/todos/{todo}', 'TodoController@destroy');
+Route::delete('/todosDeleteCompleted', 'TodoController@destroyCompleted');
